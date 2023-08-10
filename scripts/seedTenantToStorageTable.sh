c1d744a578e89e3ad01b89ee7e79c52590bb8ba4
becheng <ben.cheng@gmail.com>
2023-08-10 17:10:11 -0400

    updated script

--- scripts/seedTenantToStorageTable.sh
+++ scripts/seedTenantToStorageTable.sh
@@ -40,71 +40,77 @@ export AZURE_STORAGE_KEY="$(az storage account keys list --account-name $AZURE_S
 
 partitionKey1="storageAdapterTenants"
 
-az storage entity insert \
---entity PartitionKey=$partitionKey1 RowKey=1 \
-ConnectionUriKVSecretName="DefaultEndpointsProtocol=https;AccountName=storageadapterhack;AccountKey=4KO2FU7fBX7yYyRA4+FaMzvbJi/IDt1zw3XGqKKr8bfIxDGOOe1fP+JRae3guMF5+CA4hPYw/Sn4+AStL00c/A==;EndpointSuffix=core.windows.net" \
-ConnectionUriType="ConnectionString" \
-ContainerName="cont4tenant1" \
-CxTenantId@odata.type="Edm.Guid" CxTenantId="1cdc6cfa-0f73-4d65-a69e-86d1078b1266" \
-CxTenantName="AdventureWorks" \
-IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=false \
-StorageRegion="CanadaCentral" \
-StorageType="AzureStorageAccount" \
---if-exists replace \
---table-name $AZURE_STORAGE_TABLE_NAME
 
 az storage entity insert \
 --entity PartitionKey=$partitionKey1 RowKey=2 \
-ConnectionUriKVSecretName="https://storageadapterhack.blob.core.windows.net/cont4tenant2?sp=rawdl&st=2023-07-12T14:47:49Z&se=2023-09-09T22:47:49Z&spr=https&sv=2022-11-02&sr=c&sig=jwHiHeRdjm4aOkpt2YzbZ8dUbvePXI3uIFoVcy2iC9s%3D" \
-ConnectionUriType="SasUri" \
+StorageIdentifier="storageadapterhack" \
+StorageAccessKeySecretRef="https://storageadapterhack.blob.core.windows.net/cont4tenant2?sp=rawdl&st=2023-07-12T14:47:49Z&se=2023-09-09T22:47:49Z&spr=https&sv=2022-11-02&sr=c&sig=jwHiHeRdjm4aOkpt2YzbZ8dUbvePXI3uIFoVcy2iC9s%3D" \
+ConnectionType="AzSasUri" \
 ContainerName="cont4tenant2" \
 CxTenantId@odata.type="Edm.Guid" CxTenantId="95877230-9d77-4854-b8d0-b6c580ae8070" \
 CxTenantName="Woodgrove" \
 IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=false \
 StorageRegion="CanadaCentral" \
-StorageType="AzureStorageAccount" \
+StorageType="AzStorage" \
 --if-exists replace \
---table-name $AZURE_STORAGE_TABLE_NAME
+--table-name $AZURE_STORAGE_TABLE_NAME 
 
 az storage entity insert \
 --entity PartitionKey=$partitionKey1 RowKey=3 \
-ConnectionUriKVSecretName="https://storageadapterhack.blob.core.windows.net/" \
-ConnectionUriType="ContainerUri" \
+StorageIdentifier="storageadapterhack" \
+StorageAccessKeySecretRef="not-applicable" \
+ConnectionType="AzOauth" \
 ContainerName="cont4tenant3" \
 CxTenantId@odata.type="Edm.Guid" CxTenantId="27000a0c-8b2f-43dc-b1d2-7235ba13e0ed" \
 CxTenantName="Awesome Computers" \
 IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=false \
 StorageRegion="CanadaCentral" \
-StorageType="AzureStorageAccount" \
+StorageType="AzStorage" \
 --if-exists replace \
---table-name $AZURE_STORAGE_TABLE_NAME
+--table-name $AZURE_STORAGE_TABLE_NAME 
 
 az storage entity insert \
 --entity PartitionKey=$partitionKey1 RowKey=4 \
-ConnectionUriKVSecretName="https://sademo2t35.blob.core.windows.net/" \
-ConnectionUriType="ContainerUri" \
+StorageIdentifier="sademo2t35" \
+StorageAccessKeySecretRef="not-applicable" \
+ConnectionType="AzOauth" \
 ContainerName="feb27container" \
 CxTenantId@odata.type="Edm.Guid" CxTenantId="7f302d31-e058-4160-920b-76857bc2e284" \
 CxTenantName="Fabrikam" \
 IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=true \
 StorageRegion="CanadaCentral" \
-StorageType="AzureStorageAccount" \
+StorageType="AzStorage" \
 AzureCrossTenantDomain="benchenggmail.onmicrosoft.com" \
 AzureCrossTenantId="9e54649d-2ff3-4f06-9561-d81f12cfcfa6" \
 --if-exists replace \
---table-name $AZURE_STORAGE_TABLE_NAME
+--table-name $AZURE_STORAGE_TABLE_NAME 
 
 az storage entity insert \
 --entity PartitionKey=$partitionKey1 RowKey=5 \
-ConnectionUriKVSecretName="storageadapterhack" \
-ConnectionUriType="AWS3PresignedUrl" \
+StorageIdentifier="AKIAVYRZSLZIHY4J3H5Q" \
+StorageAccessKeySecretRef="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" \
+ConnectionType="AwsCredentials" \
 ContainerName="storageadapterhack" \
 CxTenantId@odata.type="Edm.Guid" CxTenantId="5e0a2a1f-0376-4453-875c-3d727087d607" \
 CxTenantName="adatum.com" \
 IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=false \
 StorageRegion="ca-central-1" \
-StorageType="AmazonS3" \
+StorageType="AwsS3" \
 AzureCrossTenantDomain="" \
 AzureCrossTenantId="" \
 --if-exists replace \
---table-name $AZURE_STORAGE_TABLE_NAME
+--table-name $AZURE_STORAGE_TABLE_NAME 
+
+az storage entity insert \
+--entity PartitionKey=$partitionKey1 RowKey=6 \
+StorageIdentifier="storageadapterhack" \
+StorageAccessKeySecretRef="4KO2FU7fBX7yYyRA4+FaMzvbJi/IDt1zw3XGqKKr8bfIxDGOOe1fP+JRae3guMF5+CA4hPYw/Sn4+AStL00c/A==" \
+ConnectionType="AzStorageSharedKey" \
+ContainerName="cont4tenant1" \
+CxTenantId@odata.type="Edm.Guid" CxTenantId="2b6623da-38e1-445b-80f1-5edd52f3fb7e" \
+CxTenantName="AdventureWorks" \
+IsAzureCrossTenant@odata.type="Edm.Boolean" IsAzureCrossTenant=false \
+StorageRegion="CanadaCentral" \
+StorageType="AzStorage" \
+--if-exists replace \
+--table-name $AZURE_STORAGE_TABLE_NAME 
